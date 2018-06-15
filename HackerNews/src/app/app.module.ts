86c9c07e84af9eae1edbd4f91de17ba2cba836c8
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2018-06-15 18:47:55 +0100

    add auth module with login component (simplified)

--- HackerNews/src/app/app.module.ts
+++ HackerNews/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
