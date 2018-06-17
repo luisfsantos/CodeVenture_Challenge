6760ed27073590230f8ef4494e41338e7fac217a
Luis Santos <luis.d.santos@tecnico.ulisboa.pt>
2018-06-17 21:49:55 +0100

    add jwtInterceptor from Auth0

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { JwtModule } from '@auth0/angular-jwt';
+import { HttpClientModule } from '@angular/common/http';
 import { AuthModule } from './auth/auth.module';
 import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+export function tokenGetter() {
+  return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MjkyNzEyNjIsImlhdCI6MTUyOTI2NzY2Mn0.9Og2cDQx8F9F4tYRVDe6mbQec2AMn83WgTS61F9T_yE"; //localStorage.getItem('access_token');
+}
 
 @NgModule({
   declarations: [
@@ -17,7 +22,15 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     AuthModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:4200', 'localhost:3000'],
+        blacklistedRoutes: []
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
